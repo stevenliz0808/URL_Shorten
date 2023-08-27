e7383099373380492a401d206487e75e15135d9e
tw3638993 <tw3638993@gmail.com>
2023-08-28 01:02:25 +0800

    完成版

--- app.js
+++ app.js
@@ -1,26 +1,66 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
 const { JSDOM } = require("jsdom");
-const fs = require("fs"); 
+const fs = require("fs");
 const path = require("path");
 
 const app = express();
 const port = 3000;
 
+let inputUrl = [];
+let outputUrl = [];
+
 app.use(express.static("public"));
-app.use(express.json())
+app.use(express.json());
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home")
+})
+
+app.get("/result", (req, res) => {
+  const url = req.query.inputText;
+  const letters = "abcdefghijklmnopqrustvwxyz123456789";
+  let shortenUrl = "http://localhost:3000/";
+  //避免空白
+  if (!url) {
+    res.redirect("/");
+  }
+
+  else {
+    const i = inputUrl.indexOf(url);
+    //新的網址
+    if (i === -1) {
+      let fiveLetters = "";
+      for (let i = 0; i < 5; i++) {
+        let index = Math.floor(Math.random() * 35);
+        fiveLetters += letters[index];
+      }
+      shortenUrl += fiveLetters;
+      inputUrl.push(url);
+      outputUrl.push(shortenUrl);
+      res.render("success", { shortenUrl });
+    }
+    //舊的網址
+    else {
+      shortenUrl = outputUrl[i]
+      res.render("success", { shortenUrl });
+    }
+
+    console.log(inputUrl);
+    console.log(outputUrl);
+  }
 });
 
 app.get("/:id", (req, res) => {
+  let url = "http://localhost:3000/";
   const id = req.params.id;
-  res.send(id);
+  url += id
+  let i = outputUrl.indexOf(url)
+  res.redirect(inputUrl[i]);
 });
 
 app.listen(port, () => {
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,36 +1,4 @@
-// const renderPanel = document.querySelector(".render-panel");
-
-let inputUrl = [];
-let outputUrl = [];
 let inputValue = "";
-
-function addRandomLetters(url) {
-  const letters = "abcdefghijklmnopqrustvwxyz123456789";
-  //輸入相同網址時，產生一樣的縮址
-  if (inputUrl.indexOf(url) === -1) {
-    let fiveLetters = "";
-    for (let i = 0; i < 5; i++) {
-      let index = Math.floor(Math.random() * 35);
-      fiveLetters += letters[index];
-    }
-    inputUrl.push(url);
-    outputUrl.push(`http://localhost:3000/${fiveLetters}`);
-    return `http://localhost:3000/${fiveLetters}`;
-  } else {
-    let i = inputUrl.indexOf(url);
-    return outputUrl[i];
-  }
-}
-
-function renderSuccess(url) {
-  let i = outputUrl.indexOf(url);
-  let htmlContent = `
-  <h2 class="mt-3">Success! Please use this link:</h2>
-  <a id="textToCopy" class="mb-3 text-center" href="${inputUrl[i]}">${url}</a>
-  <button type="button" class="btn btn-primary" id="btn-copy" data-clipboard-target="#textToCopy">Copy</button>
-  `;
-  $("#success").html(htmlContent);
-}
 
 $(document).ready(function () {
   $("#render-panel").on("input", "#input-url", function () {
@@ -40,33 +8,10 @@ $(document).ready(function () {
 
 $(document).ready(function () {
   $("#render-panel").on("click", "#btn-shorten", function () {
-    let url = addRandomLetters(inputValue);
     //防止空白表單送出並提示使用者
-    if (inputValue) {
-      renderSuccess(url);
-    } else {
+    if (!inputValue) {
       alert("Please enter the url");
     }
-
-    // const dataToSend = {
-    //   name: "John",
-    //   age: 30,
-    //   city: "New York",
-    // };
-    // fetch("/", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(dataToSend),
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log("Response from backend:", data);
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error:", error);
-    //   });
   });
 });
 
@@ -76,6 +21,8 @@ $(document).ready(function () {
     alert(`copy successfully`);
   });
 });
+
+// const renderPanel = document.querySelector(".render-panel");
 
 // renderPanel.addEventListener("input", (event) => {
 //   inputValue = event.target.value;
